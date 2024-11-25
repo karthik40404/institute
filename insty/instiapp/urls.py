6d998a4b307d81c180ac135f4f31d6d689337bfd
karthik40404 <karpython123@gmail.com>
2024-11-25 21:22:09 +0530

    'not_finished'

--- insty/instiapp/urls.py
+++ insty/instiapp/urls.py
@@ -2,8 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('',views.homi),
-    path('log',views.log),
-    path('admhom',views.admhom),
-    path('addc',views.addcourse),
+    path('',views.homi,name='home'),
+    path('viewcourse/<cid>',views.viewcourse,name='viewcourse'),
 ]
--- insty/instiapp/views.py
+++ insty/instiapp/views.py
@@ -1,36 +1,11 @@
-from django.shortcuts import render,redirect
-from django.contrib.auth import authenticate,login,logout
+from django.shortcuts import render
 from .models import *
 # Create your views here.
+
 def homi(req):
-    return render(req,'index.html')
-
-def admhom(req):
-    return render(req,'admin/home.html')
-
-def log(req):
-    if 'shop' in req.session:
-        return redirect (admhom)
-    if req.method=='POST':
-        uname=req.POST['uname']
-        psw=req.POST['psw']
-        data=authenticate(username=uname,password=psw)
-        print(data)
-        return redirect(log)
-    return render(req,'login.html')
-
-def addcourse(req):
-    if 'admin' in req.session:
-        if req.method=='POST':
-            cid=req.POST['cid']
-            cname=req.POST['cname']
-            disc=req.POST['disc']
-            price=req.POST['price']
-            file=req.FILES['img']
-            data=Course.objects.create(cid=cid,cname=cname,disc=disc,price=price,img=file)
-            data.save()
-            return redirect(admhom)
-        else:
-            return render(req,'admin/addc.html')
-    else:
-        return redirect(log)
+     courses = Course.objects.all()
+     return render(req,'index.html', {'courses': courses})
+ 
+def viewcourse(req,cid):
+    course=Course.objects.get(id=cid)
+    return render(req,'vcourse.html',{'courses':course})
